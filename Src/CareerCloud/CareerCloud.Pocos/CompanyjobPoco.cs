1e2b02ac507075ed87abeb99eb6c70ab8e8ab5c5
VishwantReddyMamidi <mamidivishwant@gmail.com>
2019-11-17 20:20:17 -0500

    added table columns as properties in respective table poco classes

--- Src/CareerCloud/CareerCloud.Pocos/CompanyjobPoco.cs
+++ Src/CareerCloud/CareerCloud.Pocos/CompanyjobPoco.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CareerCloud.Pocos
 {
-   public class CompanyjobPoco
+    [Table("Company_Jobs")]
+    public class CompanyJobPoco : IPoco
     {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Company { get; set; }
+        [Column("Profile_Created")]
+        public DateTime ProfileCreated { get; set; }
+        [Column("Is_Inactive")]
+        public bool IsInactive { get; set; }
+        [Column("Is_Company_Hidden")]
+        public bool IsCompanyHiddent { get; set; }
+        [Column("Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
     }
 }
